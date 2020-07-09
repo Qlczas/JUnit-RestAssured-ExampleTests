9b4096b6b3115705b9899cee7e541ff66e9e1815
Matt Cwikla <qlczas@gmail.com>
2020-07-09 23:41:10 +0200

    add Username generation

--- src/test/java/testData/endpoints/messageBoard/TestUsers.java
+++ src/test/java/testData/endpoints/messageBoard/TestUsers.java
@@ -1,8 +1,11 @@
 package testData.endpoints.messageBoard;
 
+import org.apache.commons.lang3.RandomStringUtils;
+
 public class TestUsers {
 
-    public static final String USERNAME1 = "Tester11";
+    //public static final String USERNAME1 = "Tester13";
+    public static final String USERNAME1 = RandomStringUtils.randomAlphanumeric(8).toUpperCase();
     public static final String PASSWORD1 = "Admin1";
     public static final String FIRST_NAME1 = "Testy";
     public static final String LAST_NAME1 = "Testowitz";
